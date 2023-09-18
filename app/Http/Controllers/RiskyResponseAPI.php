f08933321f0d268e2446f04a592d4705e657216d
rguttersohn <rguttersohn@cccnewyork.org>
2023-09-18 15:17:56 -0400

    refactors the get valid query

--- app/Http/Controllers/RiskyResponseAPI.php
+++ app/Http/Controllers/RiskyResponseAPI.php
@@ -7,27 +7,39 @@ use Illuminate\Support\Facades\DB;
 
 class RiskyResponseAPI extends Controller
 {
-    protected array $responses_select_list = ['rr.id','rr.year', 'rr.data','gender_constraints.label as gender', 'race_constraints.label as race', 'sexual_id_constraints.label as sexual_id', 'grade_constraints.label as grade'];
+    protected array $selections = ['rr.id','rr.year', 'rr.data','gender_constraints.label as gender', 'race_constraints.label as race', 'sexual_id_constraints.label as sexual_id', 'grade_constraints.label as grade'];
 
-    protected array $allowed_filters = ['year' => 'year',  'grade_constraints.slug' => 'grade', 'race_constraints.slug' => 'race','sexual_id_constraints.slug' => 'sexual_id', 'gender_constraints.slug' => 'sex'];
+    /** bind  query params to column 
+     * key is query param
+     * value is the associated column
+    */
+
+    protected array $allowed_queries = [
+        'id' =>'rr.id', 
+        'year' => 'year',  
+        'grade' => 'grade_constraints.slug', 
+        'race' => 'race_constraints.slug',
+        'sexual_id' => 'sexual_id_constraints.slug', 
+        'gender_id' => 'gender_constraints.slug'
+    ];
 
 
 
-    protected function getValidFilters(array $queries):array {
+    protected function getValidQueries(array $queries):array {
         
-        $filters = [];
-
+        $valid_queries = [];
+        
         foreach($queries as $key => $value):
 
-            if(array_search($key, $this->allowed_filters)):
-                
-                $filters[$key] = $value;
+            if(isset($this->allowed_queries[$key])):
+
+                $valid_queries[$key] = $value;
             
             endif;
 
         endforeach;
 
-        return $filters;
+        return $valid_queries;
 
     }
 
@@ -35,18 +47,20 @@ class RiskyResponseAPI extends Controller
         
         global $request;
 
-        $queries = $request->all();
-
-        $filters = $this->getValidFilters($queries);
+        $validated_queries = $this->getValidQueries($request->all());
         
-        $formatted_filters =[];
+        $formatted_filters = [];
 
-        foreach($filters as $key => $value):
+        foreach($validated_queries as $key => $value):
+
+            if(str_starts_with($key, 'sort_asc' || 'sort_desc')):
+                
+                continue;
             
-            $column = array_search($key, $this->allowed_filters);
+            endif;
 
-            array_push($formatted_filters, [$column, '=', $value]);
-
+            array_push($formatted_filters, [$this->allowed_queries[$key], '=', $value]);
+        
         endforeach;
 
         return $formatted_filters;
@@ -63,7 +77,7 @@ class RiskyResponseAPI extends Controller
             ->join('race_constraints', 'rr.race_constraint_id', '=', 'race_constraints.id')
             ->join('sexual_id_constraints', 'rr.sexual_id_constraint_id', '=', 'sexual_id_constraints.id')
             ->join('grade_constraints', 'rr.grade_constraint_id', '=', 'grade_constraints.id' )
-            ->select(...$this->responses_select_list)
+            ->select(...$this->selections)
             ->get()->toArray();
     }
 
@@ -75,7 +89,7 @@ class RiskyResponseAPI extends Controller
             ->join('race_constraints', 'rr.race_constraint_id', '=', 'race_constraints.id')
             ->join('sexual_id_constraints', 'rr.sexual_id_constraint_id', '=', 'sexual_id_constraints.id')
             ->join('grade_constraints', 'rr.grade_constraint_id', '=', 'grade_constraints.id' )
-            ->select(...$this->responses_select_list)
+            ->select(...$this->selections)
             ->get()->toArray();
     }
 
