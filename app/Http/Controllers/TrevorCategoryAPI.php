91860fd39b75ff6f7244ddb6a863b45e3ebd1200
rguttersohn <rguttersohn@cccnewyork.org>
2023-09-27 23:19:38 -0400

    rewrote trevor hierarchy using eloquent

--- app/Http/Controllers/TrevorCategoryAPI.php
+++ app/Http/Controllers/TrevorCategoryAPI.php
@@ -4,36 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\TrevorQuestion;
+use App\Models\TrevorCategory;
 
 class TrevorCategoryAPI extends Controller
 {
     public function getCategories():array {
-        return DB::table('trevor_categories')->select('id', 'slug', 'label')->get()->toArray();
+        return TrevorCategory::select('id', 'slug', 'label')->get()->toArray();
     }
 
     public function getCategory($env, $id):array {
-
-        $category_query = DB::table('trevor_categories')
-            ->select('id', 'slug', 'label')
-            ->where('id', '=', $id)
-            ->get()->toArray();
-
-        $questions_query = DB::table('trevor_questions as tq')
-            ->select('tq.id','question')
-            ->where('tq.trevor_category_id', '=', $id)->get()->toArray();
-
-        foreach($questions_query as &$question){
-
-            $response_query = DB::table('trevor_responses as tr')
-            ->select('tr.id','tr.data', 'tr.year')
-            ->where('tr.trevor_question_id', '=', $question->id)->get()->toArray();
-
-            $question->responses = $response_query;
-        }
-
+        
         return [
-            'category' => $category_query,
-            'questions' => $questions_query
+            'category' => TrevorCategory::select('id', 'slug', 'label')->where('id', '=', $id)->get(),
+            'questions' => TrevorQuestion::select('id', 'question','explanation', 'source_url', 'source_notes')->with('trevor_response:id,data,year,trevor_question_id')->where('trevor_category_id', '=', $id)->get()
         ];
     }
 }
--- app/Models/TrevorCategory.php
+++ app/Models/TrevorCategory.php
@@ -5,12 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class TrevorCategory extends Model
 {
     use HasFactory;
 
-    public function trevor_question ():HasMany{
-        return $this->hasMany(TrevorQuestions::class);
+    protected $table = 'trevor_categories';
+    
+    public function trevor_question():HasMany{
+        return $this->hasMany(TrevorQuestion::class);
+    }
+
+    public function trevor_response():HasManyThrough {
+        return $this->hasManyThrough(TrevorResponse::class , TrevorQuestion::class);
     }
 }
