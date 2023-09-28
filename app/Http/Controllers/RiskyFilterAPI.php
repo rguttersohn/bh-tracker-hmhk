2098be1e9909e6518c5def90f08f670b16590a99
rguttersohn <rguttersohn@cccnewyork.org>
2023-09-27 21:43:22 -0400

    adds env var to filter query

--- app/Http/Controllers/RiskyFilterAPI.php
+++ app/Http/Controllers/RiskyFilterAPI.php
@@ -8,17 +8,15 @@ use Illuminate\Support\Facades\DB;
 class RiskyFilterAPI extends Controller
 {   
 
-  
-    protected array $selection = ['id', 'slug','label', 'explanation'];
-    
-    public function getFilters(string $id){
-
+      
+    protected function getStagingFilters(string $id){
 
         $race_query = DB::table('race_constraints')->
             select('race_constraints.id', 'slug','label', 'explanation')
             ->distinct()
             ->join('risky_responses', 'race_constraints.id','=', 'race_constraint_id')
             ->where([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'staging'] ])
+            ->orWhere([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'production'] ])
             ->get()->toArray();
 
         $sex_query = DB::table('gender_constraints')
@@ -26,6 +24,7 @@ class RiskyFilterAPI extends Controller
             ->distinct()
             ->join('risky_responses', 'gender_constraints.id','=', 'gender_constraint_id')
             ->where([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'staging']])
+            ->orWhere([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'production']])
             ->get()->toArray();
 
         $sexual_id_query = DB::table('sexual_id_constraints')
@@ -33,6 +32,7 @@ class RiskyFilterAPI extends Controller
             ->distinct()
             ->join('risky_responses', 'sexual_id_constraints.id','=', 'sexual_id_constraint_id')
             ->where([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'staging']])
+            ->orWhere([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'production']])
             ->get()->toArray();
 
         return [
@@ -41,4 +41,44 @@ class RiskyFilterAPI extends Controller
             'sexual_id' => $sexual_id_query
         ];
     }
+
+    protected function getProductionFilters(string $id){
+
+        $race_query = DB::table('race_constraints')->
+            select('race_constraints.id', 'slug','label', 'explanation')
+            ->distinct()
+            ->join('risky_responses', 'race_constraints.id','=', 'race_constraint_id')
+            ->where([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'production'] ])
+            ->get()->toArray();
+
+        $sex_query = DB::table('gender_constraints')
+            ->select('gender_constraints.id', 'slug','label', 'explanation')
+            ->distinct()
+            ->join('risky_responses', 'gender_constraints.id','=', 'gender_constraint_id')
+            ->where([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'production']])
+            ->get()->toArray();
+
+        $sexual_id_query = DB::table('sexual_id_constraints')
+            ->select('sexual_id_constraints.id', 'slug','label', 'explanation')
+            ->distinct()
+            ->join('risky_responses', 'sexual_id_constraints.id','=', 'sexual_id_constraint_id')
+            ->where([['risky_responses.risky_question_id', '=', $id], ['risky_responses.publication_status', '=', 'production']])
+            ->get()->toArray();
+
+        return [
+            'race' => $race_query,
+            'sex' => $sex_query,
+            'sexual_id' => $sexual_id_query
+        ];
+    }
+
+    public function getFilters(string $env, string $id):array{
+
+        return match($env){
+            'production' => $this->getProductionFilters($id),
+            'staging' => $this->getStagingFilters($id),
+            default => [],
+         };
+    }
+
 }
