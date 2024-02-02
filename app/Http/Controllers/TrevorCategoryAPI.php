932f224cf8ef17a7ddf7b171cc76cc451b6c41c9
rguttersohn <rguttersohn@cccnewyork.org>
2024-02-02 15:18:20 -0500

    fixes bug in trevor controller

--- app/Http/Controllers/TrevorCategoryAPI.php
+++ app/Http/Controllers/TrevorCategoryAPI.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Traits\InvalidMessages;
 
 class TrevorCategoryAPI extends Controller
 {   
