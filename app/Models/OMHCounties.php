ee4e9030c984fe234cc52a5714348149c07b0ae3
rguttersohn <rguttersohn@cccnewyork.org>
2024-02-02 15:28:00 -0500

    fixes error when updating omh data

--- app/Models/OMHCounties.php
+++ app/Models/OMHCounties.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Traits\ClearCache;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class OMHCounties extends Model
 {
-    use HasFactory;
+    use HasFactory, ClearCache;
 
     protected $table = 'omh_counties';
 
