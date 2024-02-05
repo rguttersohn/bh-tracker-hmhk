efb56451b41d93a1e9b870160e307386524c4102
rguttersohn <rguttersohn@cccnewyork.org>
2024-02-05 12:34:08 -0500

    updated cors policy for api

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000', 'http://healthymindshealthykids.test'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['https:\/\/(.*\.)?healthymindshealthykids.org|https:\/\/healthymindshealthykids.org'],
 
     'allowed_headers' => ['*'],
 
