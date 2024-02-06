7ea2c7d92a2e51f8ca1d5626daec1b5ef731667d
rguttersohn <rguttersohn@cccnewyork.org>
2024-02-06 15:11:09 -0500

    adds custom error popu

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>500 Server Error</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+    <body class="h-screen w-screen flex flex-col justify-center items-center bg-green-50">
+        <h1 class="text-center text-2xl font-bold text-green-700">Server Error</h1>
+        <h2 class="text-center text-xl font-bold text-green-700">Updating the database failed.</h2>
+        <p class="text-center text-green-700">See exceptions folder to learn about what went wrong.</p>
+    </body>
+</html>
